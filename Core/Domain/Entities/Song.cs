5dd064c0ed8ad4c18caf8a1f3f6e13d5c9d5a96c
Nick Lawson <nlawson@developertown.com>
2023-01-06 23:00:51 -0500

    Fixed bug

--- Core/Domain/Entities/Song.cs
+++ Core/Domain/Entities/Song.cs
@@ -5,4 +5,11 @@ public class Song : BaseEntity
     public string Name { get; set; } = string.Empty;
     public double Length { get; set; }
     public string Lyrics { get; set; } = string.Empty;
+
+    public Song(string name, double length, string lyrics)
+    {
+        Name = name;
+        Length = length;
+        Lyrics = lyrics;
+    }
 }
